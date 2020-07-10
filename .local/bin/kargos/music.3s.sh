96f5ba89f92e3c9219cf47209f988b45ab4d0edd
jnines <jason.mullenberg@gmail.com>
2020-07-09 19:47:25 -0500

    ...

--- .local/bin/kargos/music.3s.sh
+++ .local/bin/kargos/music.3s.sh
@@ -3,7 +3,7 @@
 nplaying=$(/usr/bin/quodlibet --print-playing | sed 's/\.[^.]*$//')
 
     if [[ -n "$nplaying" ]]; then
-        echo "<body>${nplaying}&nbsp;&nbsp;&nbsp;&nbsp;</body>|iconName=audio-player size=13 color=brown bash='/usr/bin/quodlibet --show-window' onclick=bash"
+        echo "<body>${nplaying}&nbsp;&nbsp;&nbsp;&nbsp;</body>|iconName=audio-player size=13 color=grey bash='/usr/bin/quodlibet --show-window' onclick=bash"
     else
         echo "<body>&nbsp;&nbsp;&nbsp;&nbsp;</body>|iconName=audio-player bash='/usr/bin/quodlibet --start-hidden --start-playing' onclick=bash"
 
