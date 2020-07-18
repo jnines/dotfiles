898c15b583b2abce923c9ced8db21b91e94515eb
jnines <jason.mullenberg@gmail.com>
2020-07-17 20:14:25 -0500

    ...

--- .local/bin/kargos/weather.sh
+++ .local/bin/kargos/weather.sh
@@ -15,4 +15,9 @@ mapfile -t precip < <(sed '16q;d' "$wfile" | grep -wo "[0-9]*%" | sed 's/%//g')
     else
         night="🌙"
     fi
-echo "<body>${current}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${day}${tday[1]}°&nbsp;&nbsp;${night}${tday[0]}°</body> | size=15 refresh=true bash='curl -fs "wttr.in/?1" > /home/jason/.local/share/weather' onclick=bash"
+
+if [[ "${current}" =~ "Sorry"* ]]; then
+    echo "<body>* ${day}${tday[1]}°&nbsp;&nbsp;${night}${tday[0]}°</body> | size=15 refresh=true"
+else
+    echo "<body>${current}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${day}${tday[1]}°&nbsp;&nbsp;${night}${tday[0]}°</body> | size=15 refresh=true bash='curl -fs "wttr.in/?1" > /home/jason/.local/share/weather' onclick=bash"
+fi
