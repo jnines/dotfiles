77df42ae5f34640d3f4e6d256b5c0bcb2cff23ab
jnines <jason.mullenberg@gmail.com>
2020-07-28 20:37:31 -0500

    ...

--- .local/bin/kargos/headset.30.sh
+++ .local/bin/kargos/headset.30.sh
@@ -5,5 +5,5 @@ battery=$("$HOME"/.local/bin/git/headset/HeadsetControl/build/headsetcontrol -c 
     if [ "$battery" == "-1" ]; then
         echo "| iconName=audio-headset"
     else
-        echo "${battery}% | bash='"$HOME"/bin/git/headset/HeadsetControl/build/headsetcontrol -l 0' onclick=bash"
+        echo "${battery}% | bash='"$HOME"/.local/bin/git/headset/HeadsetControl/build/headsetcontrol -l 0' onclick=bash"
     fi
