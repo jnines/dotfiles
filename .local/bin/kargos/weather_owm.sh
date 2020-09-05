276b813ab7950afc5683e1d65c242f01ce96afa1
jnines <jason.mullenberg@gmail.com>
2020-09-05 18:33:11 -0500

    ...

---
+++ .local/bin/kargos/weather_owm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source "$HOME"/.config/weather.rc
+wfile="$HOME"/.local/share/weather_owm
+curl -fs "https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=minutely&units=imperial&APPID=$appid" | jq '.' > "$wfile"
+trim=('.*' '"' '"')
+IFS=$'\n'
+mapfile -t wtrd < <( jq '.current.weather[0].icon, .current.temp, .current.feels_like, .daily[0].temp.max, .daily[0].temp.min' "$wfile" )
+wtrd[3]="🔺${wtrd[3]}"
+wtrd[4]="🔻${wtrd[4]}"
+for tr in "${trim[@]}"
+do
+    wtrd=("${wtrd[@]/$tr}")
+done
+case ${wtrd[0]} in
+    01*) cur="🌞" ;;
+    02*) cur="🌤" ;;
+    03*) cur="☁" ;;
+    04*) cur="☁" ;;
+    09*) cur="🌧" ;;
+    10*) cur="🌧" ;;
+    11*) cur="⛈" ;;
+    13*) cur="🌨" ;;
+    50*) cur="🌫" ;;
+      *) cur="❗" ;;
+    esac
+
+echo "${cur} ${wtrd[1]}°/${wtrd[2]}° ${wtrd[3]}°${wtrd[4]}° | size=14 refresh=true"
+
+unset IFS
