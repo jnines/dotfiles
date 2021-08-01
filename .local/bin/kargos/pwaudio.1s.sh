bbfe144e9b227220f39879c9089583d11ae1a1ca
jnines <jason.mullenberg@gmail.com>
2021-08-01 13:11:33 -0500

    ...

--- .local/bin/kargos/pwaudio.1s.sh
+++ .local/bin/kargos/pwaudio.1s.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 speakers=$(pactl list short sinks | awk '/pci/{print $1}')
-headset=$(pactl list short sinks | awk '/Corsair/{print $1}')
+aInterface=$(pactl list short sinks | awk '/Burr/{print $1}')
 dSink=$(pactl info | grep "alsa_output.pci")
-battery=$("$HOME"/.local/bin/git/headset/HeadsetControl/build/headsetcontrol -c -b)
+aIicon="| iconName=preferences-desktop-sound"
 
-    if [ "$battery" == "-1" ] || [ "$battery" == "-2" ]; then
-        hdset="| iconName=audio-headset"
-    elif [[ "$battery" =~ [0-9] ]]; then
-        hdset="${battery}% | bash='$HOME/.local/bin/git/headset/HeadsetControl/build/headsetcontrol -l 0' onclick=bash"
-    fi
 
     if [ -z "$dSink" ]; then
-        sp="$hdset bash='pactl set-default-sink $speakers' onclick=bash"
+        sp="$aIicon bash='pactl set-default-sink $speakers' onclick=bash"
     else
-        sp="|iconName=audio-speakers size=14 bash='pactl set-default-sink $headset' onclick=bash"
+        sp="|iconName=audio-speakers size=14 bash='pactl set-default-sink $aInterface' onclick=bash"
     fi
 
 echo "${sp}"
