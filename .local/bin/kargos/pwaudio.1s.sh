c238e69097ff86a8d533f23f373dbb7791951611
jnines <jason.mullenberg@gmail.com>
2021-08-18 19:56:24 -0500

    ...

--- .local/bin/kargos/pwaudio.1s.sh
+++ .local/bin/kargos/pwaudio.1s.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+bIcon=$(pactl info | grep "Default Sink")
+case "$bIcon" in
+    *"pci"*) aIcon="audio-speakers" ;;
+    *"Burr"*) aIcon="preferences-desktop-sound" ;;
+    *"bluez"*) aIcon="bluetooth" ;;
+esac
+echo "| size=14 iconName=$aIcon"
+echo "---"
 speakers=$(pactl list short sinks | awk '/pci/{print $1}')
 aInterface=$(pactl list short sinks | awk '/Burr/{print $1}')
-dSink=$(pactl info | grep "alsa_output.pci")
-aIicon="| iconName=preferences-desktop-sound"
+bT=$(pactl list short sinks | awk '/bluez/{print $1}')
+mapfile fSinks < <(pactl list short sinks)
 
-
-    if [ -z "$dSink" ]; then
-        sp="$aIicon bash='pactl set-default-sink $speakers' onclick=bash"
-    else
-        sp="|iconName=audio-speakers size=14 bash='pactl set-default-sink $aInterface' onclick=bash"
-    fi
-
-echo "${sp}"
+for i in "${fSinks[@]}"
+do
+    case $i in
+        exit) break ;;
+        *"pci"*) echo "Speakers | iconName=audio-speakers bash='/usr/bin/pactl set-default-sink $speakers' onlclick=bash" ;;
+        *"Burr"*) echo "Interface | iconName=preferences-desktop-sound bash='/usr/bin/pactl set-default-sink $aInterface' onclick=bash" ;;
+        *"bluez"*) echo "Bluetooth | iconName=bluetooth bash='/usr/bin/pactl set-default-sink $bT' onclick=bash" ;;
+        *) echo "Unknown" ;;
+    esac
+done
