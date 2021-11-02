1198620f1acd7d71fc8c4871fcb28bfef4210a12
jnines <jason.mullenberg@gmail.com>
2021-11-02 07:25:20 -0500

    ...

--- .local/bin/kargos/cpu.3s.sh
+++ .local/bin/kargos/cpu.3s.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-temp=$(sensors | awk '/Tdie:/{gsub("+","");print $2}')
+temp=$(sensors | awk '/Tctl:/{gsub("+","");print $2}')
 echo "${temp%%.*}°| color=red size=13 iconName=amd bash='/usr/bin/plasma-systemmonitor' onclick=bash"
