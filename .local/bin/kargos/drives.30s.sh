c1b5a1e0fbfcd167768a8839dc60356f89199736
jnines <jason.mullenberg@gmail.com>
2023-01-25 10:25:54 -0600

    ...

--- .local/bin/kargos/drives.30s.sh
+++ .local/bin/kargos/drives.30s.sh
@@ -2,6 +2,6 @@
 
 nvme0=$(sensors | grep -oP 'Composite.*?\+\K[0-9.]+' | head -n 1)
 nvme1=$(sensors | grep -oP 'Composite.*?\+\K[0-9.]+' | tail -n 1)
-hdd=$(sensors | grep -oP 'temp1.*?\+\K[0-9.]+' | head -n 1)
+hdd=$(sudo hddtemp /dev/sdb | awk '{print $3}' | cut -c -2)
 
 echo "<body>${nvme0%%.*}° ${nvme1%%.*}° ${hdd%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
--- .local/bin/kargos/dryer.sh
+++ .local/bin/kargos/dryer.sh
@@ -4,7 +4,7 @@ source "$HOME"/.config/ha.rc
 dryer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" $haUrl/api/states/sensor.dryer_time_display | jq '.state' | tr -d \")
 
 if [[ -z $dryer ]]; then
-    echo "| imageURL=$HOME/.local/bin/kargos/dryer.png"
+    echo "| imageURL=$HOME/.local/bin/kargos/dryer.png size=15"
 else
-    echo "$(echo "$dryer" | awk -F ':' '{print " 0"$1":"$2}') | imageURL=$HOME/.local/bin/kargos/dryer.png" size=14
+    echo "$(echo "$dryer" | awk -F ':' '{print " 0"$1":"$2}') | imageURL=$HOME/.local/bin/kargos/dryer.png  size=14"
 fi
