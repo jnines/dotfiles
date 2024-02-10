c95f3f8098daea3c4d734b60ebebd05a97f98329
jnines <jason.mullenberg@gmail.com>
2024-02-10 10:47:33 -0600

    ...

--- .local/bin/kargos/weather_wund.sh
+++ .local/bin/kargos/weather_wund.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source "$HOME"/.config/weather.rc
 
-mapfile -t wCur < <( curl -fs "https://api.weather.com/v2/pws/observations/current?stationId=$station&format=json&units=e&apiKey=$api_key" |
-jq '.observations[0].imperial.temp, .observations[0].imperial.heatIndex, .observations[0].imperial.windChill')
+mapfile -t wCur < <(curl -fs "https://api.weather.com/v2/pws/observations/current?stationId=$station&format=json&units=e&apiKey=$api_key" |
+	jq '.observations[0].imperial.temp, .observations[0].imperial.heatIndex, .observations[0].imperial.windChill')
 
 if ((wCur[0] >= 60)); then
-    echo "${wCur[0]}°/${wCur[1]}° | size=15 refresh=true"
+	echo "${wCur[0]}°/${wCur[1]}° | size=14 refresh=true"
 else
-    echo "${wCur[0]}°/${wCur[2]}° | size=15 refresh=true"
+	echo "${wCur[0]}°/${wCur[2]}° | size=14 refresh=true"
 fi
