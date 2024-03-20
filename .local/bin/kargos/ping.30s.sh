7a7fbf88c8b3aa87bd3a1c6cc334f3a221992294
jnines <jason.mullenberg@gmail.com>
2024-03-20 17:00:56 -0500

    ...

--- .local/bin/kargos/ping.30s.sh
+++ .local/bin/kargos/ping.30s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "$(ping -c 4 1.1.1.1 | tail -1 | awk '{print $4}' | cut -d '/' -f 2 | xargs printf "%4.fms") | size=13"
+echo "$(ping -c 4 1.1.1.1 | tail -1 | awk '{print $4}' | cut -d '/' -f 2 | xargs printf "%4.fms") | size=13 color='white'"
--- .local/bin/kargos/weather_wund.sh
+++ .local/bin/kargos/weather_wund.sh
@@ -5,7 +5,7 @@ mapfile -t wCur < <(curl -fs "https://api.weather.com/v2/pws/observations/curren
 	jq '.observations[0].imperial.temp, .observations[0].imperial.heatIndex, .observations[0].imperial.windChill')
 
 if ((wCur[0] >= 60)); then
-	echo "${wCur[0]}°/${wCur[1]}° | size=14 refresh=true"
+	echo "${wCur[0]}°/${wCur[1]}° | size=14 refresh=true color='white'"
 else
-	echo "${wCur[0]}°/${wCur[2]}° | size=14 refresh=true"
+	echo "${wCur[0]}°/${wCur[2]}° | size=14 refresh=true color='white'"
 fi
