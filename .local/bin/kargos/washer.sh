dd961a82e807a6cbbb2fb6413d46107d5c8f4289
jnines <jason.mullenberg@gmail.com>
2024-03-21 17:16:49 -0500

    ...

--- .local/bin/kargos/washer.sh
+++ .local/bin/kargos/washer.sh
@@ -4,7 +4,8 @@ source "$HOME"/.config/ha.rc
 washer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" $haUrl/api/states/sensor.washer_time_display | jq '.state' | tr -d \")
 
 if [[ -z $washer ]]; then
-    echo "| imageURL=$HOME/.local/bin/kargos/washer.png size=15"
+	echo "| imageURL=$HOME/.local/bin/kargos/washer.png size=15"
 else
-    echo "$(echo "$washer" | awk -F ':' '{print " 0"$1":"$2}') | imageURL=$HOME/.local/bin/kargos/washer.png size=14"
+	echo "$(echo "$washer" | awk -F ':' '{print " 0"$1":"$2}') | imageURL=$HOME/.local/bin/kargos/washer.png size=14 color='white'"
 fi
+
