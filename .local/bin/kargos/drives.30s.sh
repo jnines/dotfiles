1edcfdde440abbf52395288cbdc19cf48e7b67a5
jnines <jason.mullenberg@gmail.com>
2024-08-04 12:41:00 -0500

    ...

--- .local/bin/kargos/drives.30s.sh
+++ .local/bin/kargos/drives.30s.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-nvme0=$(sensors | grep -oP 'Composite.*?\+\K[0-9.]+' | head -n 1)
-nvme1=$(sensors | grep -oP 'Composite.*?\+\K[0-9.]+' | tail -n 1)
+sensor_data=$(sensors)
 
-echo "<body>${nvme0%%.*}° ${nvme1%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
+nvme0=$(grep -oP 'Composite.*?\+\K[0-9.]+' <<<"$sensor_data" | head -n 1)
+nvme1=$(grep -oP 'Composite.*?\+\K[0-9.]+' <<<"$sensor_data" | tail -n 1)
+ssd=$(awk '/drivetemp-scsi-5-0/,/^$/' <<<"$sensor_data" | grep 'temp1' | awk '{print $2}' | tr -d '+')
+
+echo "<body>${nvme0%%.*}° ${nvme1%%.*}° ${ssd%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
