741cc7f65e301621ddc3be720e7f6dae52bd56c0
jnines <jason.mullenberg@gmail.com>
2024-08-04 10:02:00 -0500

    ...

--- .local/bin/kargos/drives.30s.sh
+++ .local/bin/kargos/drives.30s.sh
@@ -2,6 +2,5 @@
 
 nvme0=$(sensors | grep -oP 'Composite.*?\+\K[0-9.]+' | head -n 1)
 nvme1=$(sensors | grep -oP 'Composite.*?\+\K[0-9.]+' | tail -n 1)
-hdd=$(nc localhost 7634 | awk -F '|' '{print $4}')
 
-echo "<body>${nvme0%%.*}° ${nvme1%%.*}° ${hdd%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
+echo "<body>${nvme0%%.*}° ${nvme1%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
