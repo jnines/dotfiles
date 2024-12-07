98c95635785d10cd5027c302600e04f12ccc2250
jnines <jason.mullenberg@gmail.com>
2024-12-07 11:48:05 -0600

    ...

--- .local/bin/kargos/drives.30s.sh
+++ .local/bin/kargos/drives.30s.sh
@@ -2,8 +2,9 @@
 
 sensor_data=$(sensors)
 
-nvme0=$(grep -oP 'Composite.*?\+\K[0-9.]+' <<<"$sensor_data" | head -n 1)
-nvme1=$(grep -oP 'Composite.*?\+\K[0-9.]+' <<<"$sensor_data" | tail -n 1)
+nvme0=$(grep -oP 'SHPP41-2000GM.*?\+\K[0-9.]+' <<<"$sensor_data" | head -n 1)
+nvme1=$(grep -oP 'Sabrent.*?\+\K[0-9.]+' <<<"$sensor_data" | head -n 1)
+nvme2=$(grep -oP 'SN850X.*?\+\K[0-9.]+' <<<"$sensor_data" | head -n 1)
 ssd=$(awk '/drivetemp-scsi-2-0/,/^$/' <<<"$sensor_data" | grep 'temp1' | awk '{print $2}' | tr -d '+')
 
-echo "<body>${nvme0%%.*}° ${nvme1%%.*}° ${ssd%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
+echo "<body>${nvme0%%.*}° ${nvme1%%.*}° ${nvme2%%.*}° ${ssd%%.*}° &nbsp;</body> | size=13 color=orange iconName=drive-harddisk-system"
