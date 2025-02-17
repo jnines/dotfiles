71074cc2667e8e078c3f3799c532cb15bc7a8aec
jnines <jason.mullenberg@gmail.com>
2025-02-17 17:45:38 -0600

    ...

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,25 +1,21 @@
 function Linemode:my_data()
 	local year = os.date("%Y")
-	local time = (self._file.cha.modified or 0) // 1
-
+	local time = (self._file.cha.mtime or 0) // 1
+	local dtime
 	if time > 0 and os.date("%Y", time) == year then
-		time = os.date("%b %d %I:%M%p", time)
+		dtime = os.date("%b %d %I:%M%p", time)
 	else
-		time = time and os.date("%b %d  %Y", time) or ""
+		dtime = time and os.date("%b %d  %Y", time) or ""
 	end
 
 	local size = self._file:size()
 	local uname = self._file.cha.uid
-	local gname = self._file.cha.gid
-	local permissions = self._file.cha:permissions()
-	return ui.Line(
-		string.format(
-			"%s:%s %s %s %s ",
-			ya.user_name(uname),
-			ya.group_name(gname),
-			permissions,
-			size and ya.readable_size(size) or "-",
-			time
-		)
+	local permissions = self._file.cha:perm()
+	return string.format(
+		"%s %s %s %s ",
+		ya.user_name(uname),
+		permissions,
+		size and ya.readable_size(size) or "-",
+		dtime
 	)
 end
