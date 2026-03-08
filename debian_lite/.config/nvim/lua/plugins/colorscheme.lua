0b69011e12440257e1c7fac3ed09dd5d78a4e6e5
jnines <jason.mullenberg@gmail.com>
2026-03-08 10:35:46 -0500

    Fix snacks breaking on missing hl

--- debian_lite/.config/nvim/lua/plugins/colorscheme.lua
+++ debian_lite/.config/nvim/lua/plugins/colorscheme.lua
@@ -16,5 +16,9 @@ return {
     vim.api.nvim_set_hl(0, 'SpellBad', { underline = true, sp = 'DarkYellow' })
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NonText', { fg = '#6b625d' })
+    -- fix snacks
+    vim.api.nvim_set_hl(0, 'DiffAdd', { fg = '#DDC7A1', bg = '#32361a', ctermbg = 22 })
+    vim.api.nvim_set_hl(0, 'DiffDelete', { fg = '#DDC7A1', bg = '#3c1f1e', ctermbg = 52 })
+    vim.api.nvim_set_hl(0, 'DiffChange', { fg = '#DDC7A1', bg = '#d31380', ctermbg = 17 })
   end,
 }
