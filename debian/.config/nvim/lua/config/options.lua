84b0c1729a7a3d9bf76f5edd084baefdd651c0ac
jnines <jason.mullenberg@gmail.com>
2026-03-20 10:06:35 -0500

    Get copy to local clipboard working through tmux/ghostty

--- debian/.config/nvim/lua/config/options.lua
+++ debian/.config/nvim/lua/config/options.lua
@@ -58,3 +58,18 @@ o.foldenable = true
 o.foldlevel = 99
 o.foldmethod = 'expr'
 o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+
+if vim.env.SSH_CLIENT or vim.env.SSH_TTY then
+  local _, osc52 = pcall(require, 'vim.ui.clipboard.osc52')
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = osc52.copy('+'),
+      ['*'] = osc52.copy('*'),
+    },
+    paste = {
+      ['+'] = osc52.paste('+'),
+      ['*'] = osc52.paste('*'),
+    },
+  }
+end
