1581662cb6d7eee1bbc47628a827fe3cc3448615
jnines <jason.mullenberg@gmail.com>
2026-05-21 18:38:54 -0500

    Update washer/dryer for new api

--- arch/.local/bin/co/washer-dryer.sh
+++ arch/.local/bin/co/washer-dryer.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
+# shellcheck disable=SC1091,SC2154,SC2323
 source "$HOME"/.config/ha.rc
 
-dryer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" $haUrl/api/states/sensor.dryer_time_display | jq '.state' | tr -d \")
-washer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" $haUrl/api/states/sensor.washer_time_display | jq '.state' | tr -d \")
+dryer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" "$haUrl"/api/states/sensor.dryer_remaining_time_2 | jq '.state' | tr -d \")
+washer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" "$haUrl"/api/states/sensor.washer_remaining_time_2 | jq '.state' | tr -d \")
 
-if [[ -z $washer ]]; then
+if [[ -z $washer ]] || [[ $washer == u* ]]; then
 	wash_img="󰜪 "
-elif [[ $washer == u* ]]; then
-	wash_img="󰜪 -- "
 else
-	wash_time=$(awk -F ':' '{print " 0"$1":"$2}' <<<"$washer")
+	wash_secs=$((($(date -d "$washer" +%s) - $(date +%s))))
+	wash_time=$(date -u -d "@$wash_secs" +%H:%M)
 	wash_img="󰜪 $wash_time "
 fi
 
-if [[ -z $dryer ]]; then
+if [[ -z $dryer ]] || [[ $dryer == u* ]]; then
 	dry_img="󰤗"
-elif [[ $dryer == u* ]]; then
-	dry_img="󰤗 --"
 else
-	dryer_time=$(awk -F ':' '{print " 0"$1":"$2}' <<<"$dryer")
+	dryer_secs=$((($(date -d "$dryer" +%s) - $(date +%s))))
+	dryer_time=$(date -u -d "@$dryer_secs" +%H:%M)
 	dry_img="󰤗 $dryer_time"
 fi
 
