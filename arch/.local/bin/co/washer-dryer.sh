365889a5a9f3e9c4e5919b56c861246dd07a9410
jnines <jason.mullenberg@gmail.com>
2026-06-13 17:52:00 -0500

    Update washer/dryer co

--- arch/.local/bin/co/washer-dryer.sh
+++ arch/.local/bin/co/washer-dryer.sh
@@ -2,23 +2,19 @@
 # shellcheck disable=SC1091,SC2154,SC2323
 source "$HOME"/.config/ha.rc
 
-dryer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" "$haUrl"/api/states/sensor.dryer_remaining_time_2 | jq '.state' | tr -d \")
-washer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" "$haUrl"/api/states/sensor.washer_remaining_time_2 | jq '.state' | tr -d \")
+dryer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" "$haUrl"/api/states/sensor.dtimer | jq '.state' | tr -d \")
+washer=$(/usr/bin/curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" "$haUrl"/api/states/sensor.wtimer | jq '.state' | tr -d \")
 
-if [[ -z $washer ]] || [[ $washer == u* ]]; then
+if [[ $washer == "Off" ]]; then
 	wash_img="󰜪 "
 else
-	wash_secs=$((($(date -d "$washer" +%s) - $(date +%s))))
-	wash_time=$(date -u -d "@$wash_secs" +%H:%M)
-	wash_img="󰜪 $wash_time "
+	wash_img="󰜪 $washer"
 fi
 
-if [[ -z $dryer ]] || [[ $dryer == u* ]]; then
+if [[ $dryer == "Off" ]]; then
 	dry_img="󰤗"
 else
-	dryer_secs=$((($(date -d "$dryer" +%s) - $(date +%s))))
-	dryer_time=$(date -u -d "@$dryer_secs" +%H:%M)
-	dry_img="󰤗 $dryer_time"
+	dry_img="󰤗 $dryer"
 fi
 
 echo "$wash_img $dry_img"
